8c1f4a2f67c3dcd803687b4b80feb95f59c59a83
abuhanaan <adewunmi99@gmail.com>
2022-01-08 00:42:40 +0100

    Disabling password when connecting to server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Connecting to my server without typing password.
 ssh -i ~/.ssh/school ubuntu@34.75.15.70
-sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/ssh_config
+sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/ssh_config
