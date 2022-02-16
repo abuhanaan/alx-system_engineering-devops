ed44f87a8c2cef06e916a93846a027d4aa0a1aef
abuhanaan <adewunmi99@gmail.com>
2022-02-16 03:32:21 +0100

    Prints title of 1st 10 hottest posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     params = {
         "limit": 10
